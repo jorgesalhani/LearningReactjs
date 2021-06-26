f9dd990ec516d0334ca8a0f0d0a51283edfe8a64
jorgesalhani <jorge.salhani@usp.br>
2021-06-26 17:51:17 -0300

    scrimba tutorial

---
+++ scrimba-tutorial/src/components/MyInfo.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+
+function MyInfo() {
+    return (
+        <div>
+            <h1>Jorge Salhani</h1>
+            <p>Hello, me!</p>
+            <ol>
+                <li>Vac 1</li>
+                <li>Vac 2</li>
+                <li>Vac 3</li>
+            </ol>
+        </div>
+    )
+}
+
+export default MyInfo
---
+++ scrimba-tutorial/src/index.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// ===============
+// ReactDOM.render(
+//     <h1>Hello World!</h1>,
+//     document.getElementById("root")
+// )
+
+// ===============
+// function MyApp() {
+//     return ( 
+//     <ul>
+//         <li>1</li>
+//         <li>2</li>
+//         <li>3</li>
+//     </ul>
+//     )
+// }
+
+// ReactDOM.render(
+//     <MyApp />,
+//     document.getElementById("root")
+// )
+
+// ===============
+// function MyInfo() {
+//     return (
+//         <div>
+//             <h1>Jorge Salhani</h1>
+//             <p>Hello, me!</p>
+//             <ol>
+//                 <li>Vac 1</li>
+//                 <li>Vac 2</li>
+//                 <li>Vac 3</li>
+//             </ol>
+//         </div>
+//     )
+// }
+
+// ReactDOM.render(
+//     <MyInfo />,
+//     document.getElementById("root")
+// )
+
+// ================
+// import MyInfo from "./components/MyInfo.js"
+
+// ReactDOM.render(
+//     <MyInfo />,
+//     document.getElementById("root")
+// )
+
+// ================
+// import App from "./App.js"
+
+// ReactDOM.render(
+//     <App />,
+//     document.getElementById("root")
+// )
+
+// ================
+
